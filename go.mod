98a7d4323bafd3a512dcb64c7c10a2e7b98fb4d1
Justin Biviano <just.biviano@gmail.com>
2026-09-05 21:09:40 +1000

    Working GO model with function calling and neatJSON formatting

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.27.0
 require github.com/cloudflare/cloudflare-go/v7 v7.10.0
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- main.go
+++ main.go
@@ -2,18 +2,41 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/cloudflare/cloudflare-go/v7"
 	"github.com/cloudflare/cloudflare-go/v7/dns"
 	"github.com/cloudflare/cloudflare-go/v7/option"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load env file relying on system env.")
+	}
+
+	recordResponse, err := UpdateDNSRecord(os.Getenv("IP_ADDRESS"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	neatJSON, err := json.MarshalIndent(recordResponse, "", "	")
+	if err != nil {
+		log.Printf("Failed to neaten JSON: %w", err)
+	}
+
+	fmt.Print(string(neatJSON))
+}
+
+func UpdateDNSRecord(NewIPAddress string) (*dns.RecordResponse, error) {
 	client := cloudflare.NewClient(
 		option.WithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN")),
 	)
+
 	recordResponse, err := client.DNS.Records.Update(
 		context.TODO(),
 		os.Getenv("DNS_RECORD_ID"),
@@ -23,12 +46,14 @@ func main() {
 				Name:    cloudflare.F(os.Getenv("DOMAIN_NAME")),
 				TTL:     cloudflare.F(dns.TTL1),
 				Type:    cloudflare.F(dns.ARecordTypeA),
-				Content: cloudflare.F("1.42.114.177"),
+				Content: cloudflare.F(NewIPAddress),
 			},
 		},
 	)
+
 	if err != nil {
-		panic(err.Error())
+		return nil, fmt.Errorf("Failed to update DNS record: %w", err)
 	}
-	fmt.Printf("%+v\n", recordResponse)
+
+	return recordResponse, nil
 }
